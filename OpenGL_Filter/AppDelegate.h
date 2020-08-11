049b24a8887b341bcf2636dc912577d2158d1b79
wangqiao <984361822@qq.com>
2020-08-11 15:57:47 +0800

    first commit

---
+++ OpenGL_Filter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenGL_Filter
+//
+//  Created by zhangchi on 8/11/20.
+//  Copyright © 2020 Wangqiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ OpenGL_Filter/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  OpenGL_Filter
+//
+//  Created by zhangchi on 8/11/20.
+//  Copyright © 2020 Wangqiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
